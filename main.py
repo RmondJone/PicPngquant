001124a4cc1928e302c61d7bad7803aec5ffa037
guohanlin <2318560278@qq.com>
2022-03-30 11:31:47 +0800

    [Bug修复](master): 兼容新版pngquant

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 import os
 import threading
 
+
 # 压缩线程（同步压缩）
 class CompressThread(threading.Thread):
     # 构造方法
-    def __init__(self, compressDir) -> None:
+    def __init__(self, rootPath, compressFile, compressPath, extensionName) -> None:
         threading.Thread.__init__(self)
-        self.compressDir = compressDir
+        self.root = rootPath
+        self.compressFile = compressFile
+        self.path = compressPath
+        self.extension = extensionName
 
     # 运行方法
     def run(self) -> None:
-        print("线程开始运行，压缩路径为：" + self.compressDir)
+        print("\n线程开始运行，压缩图片路径为：" + self.path)
         # 获得锁
         threadLock.acquire()
-        cmd = "pngquant 256 --quality=65-80 --skip-if-larger --force --ext .png " + self.compressDir + "\\*.png"
+        cmd = "pngquant 256 --quality=65-80 --skip-if-larger --force --ext .png " + self.path
         os.system(cmd)
+        # 重命名后缀
+        if self.extension == 'jpg' or self.extension == 'jpeg':
+            os.remove(self.path)
+            os.rename(os.path.join(self.root, self.compressFile + ".png"),
+                      os.path.join(self.root, self.compressFile))
         # 释放锁
         threadLock.release()
-        print("线程结束运行，压缩路径为：" + self.compressDir)
+        print("\n线程结束运行，压缩图片路径为：" + self.path)
 
 
 if __name__ == '__main__':
-    configDirStr = "drawable-hdpi drawable-xhdpi drawable-xxhdpi drawable-xxxhdpi mipmap-hdpi mipmap-xhdpi mipmap-xxhdpi mipmap-xxxhdpi"
-    configDir = configDirStr.split(" ")
-    print("当前配置需要压缩的文件夹配置为：")
-    print(configDir)
-    a = """
+    tag = """
  _____ _             _____                                     _   
 |  __ (_)           |  __ \                                   | |  
 | |__) |  ___ ______| |__) | __   __ _  __ _ _   _  __ _ _ __ | |_ 
@@ -35,28 +40,35 @@ if __name__ == '__main__':
                                   __/ |   | |                      
                                  |___/    |_|     
 """
-print(a)
-changeConfig = input("是否需要更改默认压缩文件夹配置(Y/N)：")
-if changeConfig == "Y":
-    configDirStr = input("请输入需要压缩的文件夹配置(多个以空格分隔)：")
-    configDir = configDirStr.split(" ")
-    print("当前配置需要压缩的文件夹配置为：")
-    print(configDir)
-dirPath = input("请选择需要压缩的文件夹路径：")
-# 初始化线程锁
-threadLock = threading.Lock()
-# 压缩线程数组
-threads = []
-# 开始历遍所有子文件夹
-for root, dirs, files in os.walk(dirPath):
-    for dir in dirs:
-        if dir in configDir:
-            # 过滤编译文件夹
-            if "build\generated" not in os.path.join(root, dir):
-                thread = CompressThread(os.path.join(root, dir))
-                threads.append(thread)
-                thread.start()
+print(tag)
 
-# 开始遍历执行压缩线程
-for thread in threads:
-    thread.join()
+
+# 创建压缩线程
+def addThread(rootPath, compressFile, compressPath, extensionName):
+    compressThread = CompressThread(rootPath, compressFile, compressPath, extensionName)
+    compressThread.start()
+    threads.append(compressThread)
+
+
+if os.system("pngquant --version") != 0:
+    print("\n未检测到pngquant命令行环境，请参照pngquant官网搭建命令行环境：https://pngquant.org/")
+else:
+    dirPath = input("请选择需要压缩的文件夹路径：")
+    # 初始化线程锁
+    threadLock = threading.Lock()
+    # 压缩线程数组
+    threads = []
+    # 开始历遍所有图片
+    for root, dirs, files in os.walk(dirPath):
+        # 当前路径下所有的图片加入压缩线程
+        for childFile in files:
+            # 文件名
+            childFilePath = os.path.join(root, childFile)
+            # 扩展名
+            extension = os.path.splitext(childFilePath)[1][1:]
+            if extension == 'png' or extension == 'jpg' or extension == 'jpeg':
+                addThread(root, childFile, childFilePath, extension)
+
+    # 开始遍历执行压缩线程
+    for thread in threads:
+        thread.join()
