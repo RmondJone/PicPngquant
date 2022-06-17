ed2eac54f185c96e91738dbd28ea3ea37408e206
郭翰林 <guohanlin.ghl@ncarzone.com>
2022-06-17 17:52:14 +0800

    [Bug修复]: 修复文件夹中有大量图片时程序卡死问题

--- main.py
+++ main.py
@@ -1,33 +1,18 @@
 import os
 import platform
-import threading
 
 
-# 压缩线程（同步压缩）
-class CompressThread(threading.Thread):
-    # 构造方法
-    def __init__(self, rootPath, compressFile, compressPath, extensionName) -> None:
-        threading.Thread.__init__(self)
-        self.root = rootPath
-        self.compressFile = compressFile
-        self.path = compressPath
-        self.extension = extensionName
-
-    # 运行方法
-    def run(self) -> None:
-        print("\n线程开始运行，压缩图片路径为：" + self.path)
-        # 获得锁
-        threadLock.acquire()
-        cmd = "pngquant 256 --quality=65-80 --skip-if-larger --force --ext .png " + self.path
-        os.system(cmd)
-        # 重命名后缀
-        if self.extension == 'jpg' or self.extension == 'jpeg':
-            os.remove(self.path)
-            os.rename(os.path.join(self.root, self.compressFile + ".png"),
-                      os.path.join(self.root, self.compressFile))
-        # 释放锁
-        threadLock.release()
-        print("\n线程结束运行，压缩图片路径为：" + self.path)
+# 开始压缩图片
+def startCompress(root, compressFile, path, extension):
+    print("\n压缩开始运行，压缩图片路径为：" + path)
+    cmd = "pngquant 256 --quality=65-80 --skip-if-larger --force --ext .png " + path
+    os.system(cmd)
+    # 重命名后缀
+    if extension == 'jpg' or extension == 'jpeg':
+        os.remove(path)
+        os.rename(os.path.join(root, compressFile + ".png"),
+                  os.path.join(root, compressFile))
+    print("\n压缩结束运行，压缩图片路径为：" + path)
 
 
 if __name__ == '__main__':
@@ -50,14 +35,6 @@ if isNeedExclude == "Y" or isNeedExclude == "y":
     print("当前配置的排除压缩文件夹为：")
     print(excludeDir)
 
-
-# 创建压缩线程
-def addThread(rootPath, compressFile, compressPath, extensionName):
-    compressThread = CompressThread(rootPath, compressFile, compressPath, extensionName)
-    compressThread.start()
-    threads.append(compressThread)
-
-
 if os.system("pngquant --version") != 0:
     print("\n未检测到pngquant命令行环境，请参照pngquant官网搭建命令行环境：https://pngquant.org/")
 else:
@@ -66,10 +43,6 @@ else:
     dirPath = dirPath.rstrip()
     dirPath = dirPath.lstrip()
     print(dirPath)
-    # 初始化线程锁
-    threadLock = threading.Lock()
-    # 压缩线程数组
-    threads = []
     # 开始历遍所有图片
     for root, dirs, files in os.walk(dirPath):
         # 当前路径下所有的图片加入压缩线程
@@ -83,11 +56,8 @@ else:
                 extension = os.path.splitext(childFilePath)[1][1:]
                 if platform.system() != 'Windows':
                     if extension == 'png' or extension == 'jpg' or extension == 'jpeg':
-                        addThread(root, childFile, childFilePath, extension)
+                        startCompress(root, childFile, childFilePath, extension)
                 else:
                     # Windows版pngquant只支持png压缩
                     if extension == 'png':
-                        addThread(root, childFile, childFilePath, extension)
-    # 开始遍历执行压缩线程
-    for thread in threads:
-        thread.join()
+                        startCompress(root, childFile, childFilePath, extension)
